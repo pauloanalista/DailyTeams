e1aa8a6f6718f9f053fc3d4901ecb96a47310468
Paulo Rogerio Martins Marques <paulo.marques@uniodigital.com.br>
2022-08-05 13:24:36 -0300

    Add project

---
+++ UnioDailyTeams/Infrastructure/ServiceBaseLifetime.cs
@@ -0,0 +1,49 @@
+﻿using FluentScheduler;
+using System;
+using System.Collections.Generic;
+using Teams.Notifications;
+using Topshelf;
+
+namespace UnioDailyTeams.Infrastructure
+{
+    public class ServiceBaseLifetime : ServiceControl
+    {
+        TeamsNotificationClient _client = new TeamsNotificationClient("https://uniosolucoestecnologia.webhook.office.com/webhookb2/b320a924-1180-443f-9a0f-c3456d723331@29600058-6b0a-4b65-9f5f-093c0959a810/IncomingWebhook/cc1144e1c8594e2cbb0966e994ddd3cf/15aa2b0b-56b0-49d2-b8f3-30452d14ea24");
+        public bool Start(HostControl hostControl)
+        {
+
+            JobManager.Initialize();
+            JobManager.AddJob(
+
+                () => EnviarMensagem(),
+                s => s.NonReentrant()
+                      .ToRunEvery(1)
+                      .Days()
+                      .At(8, 59)
+            );
+
+            return true;
+        }
+
+
+        public bool Stop(HostControl hostControl)
+        {
+            JobManager.Stop();
+            return true;
+        }
+
+        public void EnviarMensagem()
+        {
+            var message = new MessageCard();
+            message.Title = "Daily " + DateTime.Now.ToString("dd/MM/yyyy");
+            message.Text = "Bom dia, procure ser objetivo em sua daily de hoje.";
+            message.Color = "f0ad4e";
+            message.Sections = new List<MessageSection>();
+            message.Sections.Add(new MessageSection() { Title = "O que você fez ontem?" });
+            message.Sections.Add(new MessageSection() { Title = "O que vai fazer hoje?" });
+            message.Sections.Add(new MessageSection() { Title = "Tem algum impedimento?" });
+
+            _client.PostMessage(message).GetAwaiter().GetResult();
+        }
+    }
+}
---
+++ UnioDailyTeams/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using UnioDailyTeams.Infrastructure;
+using Topshelf;
+
+namespace UnioDailyTeams
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var host = new HostBuilder().
+                UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureServices((hostBuilderContext, services) =>
+                {
+                    services.AddSingleton(typeof(ServiceBaseLifetime));
+                });
+
+            HostFactory.Run(x =>
+            {
+                x.Service<ServiceBaseLifetime>(sc =>
+                {
+                    sc.ConstructUsing(s => host.Build().Services.GetRequiredService<ServiceBaseLifetime>());
+                    sc.WhenStarted((s, c) => s.Start(c));
+                    sc.WhenStopped((s, c) => s.Stop(c));
+                });
+
+                x.RunAsLocalSystem()
+                    .DependsOnEventLog()
+                    .StartAutomatically()
+                    .EnableServiceRecovery(rc => rc.RestartService(1));
+
+                x.SetDescription("Envia aviso de reunião diaria.");
+                x.SetDisplayName("Unio Daily Teams");
+                x.SetServiceName("Unio Daily Teams");
+            });
+
+            await Task.CompletedTask;
+        }
+    }
+}
